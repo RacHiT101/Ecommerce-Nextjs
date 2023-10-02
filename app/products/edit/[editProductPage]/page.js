8dabda23283e42f082ed722dc6f10485b275b123
RacHiT101 <rachitgala05@gmail.com>
2023-10-02 10:10:05 +0530

    no changes

--- app/products/edit/[editProductPage]/page.js
+++ app/products/edit/[editProductPage]/page.js
@@ -10,7 +10,7 @@ const EditProductPage = ({ params }) => {
 
   const getProduct = async () => {
     const res = await axios.get("/api/products?id=" + params.editProductPage);
-    // console.log(res.data);
+    console.log(res.data);
     setProductInfo(res.data)
   };
 
